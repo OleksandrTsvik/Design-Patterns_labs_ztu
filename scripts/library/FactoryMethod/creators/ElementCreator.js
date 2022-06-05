31e060f3a53c59f81af37848d580fa5729e086e1
Oleksandr Tsvik <oleksandr.zwick@gmail.com>
2022-06-06 00:45:22 +0300

    factory

--- scripts/library/FactoryMethod/creators/ElementCreator.js
+++ scripts/library/FactoryMethod/creators/ElementCreator.js
@@ -2,8 +2,12 @@ export class ElementCreator {
     create(params) { }
 
     createAndAppendTo(domElement, params) {
+        domElement.appendChild( this.getElement(params) );
+    }
+
+    getElement(params) {
         let element = this.create(params);
         element.build();
-        domElement.appendChild(element.getDOMElement());
+        return element.getDOMElement();
     }
 }
