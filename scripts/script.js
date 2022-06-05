dcfb4e3d33e8da2eac3ebf64876ade563c15ee4c
Oleksandr Tsvik <oleksandr.zwick@gmail.com>
2022-06-06 00:48:47 +0300

    Demo class used

--- scripts/script.js
+++ scripts/script.js
@@ -1,35 +1,12 @@
-import { NewsElementCreator } from "./library/FactoryMethod/creators/NewsElementCreator.js";
-import { UserElementCreator } from "./library/FactoryMethod/creators/UserElementCreator.js";
-import { FormElementCreator } from "./library/FactoryMethod/creators/FormElementCreator.js";
-
 import { Core } from "./library/Singleton/Core.js";
-
+import { Demo } from "./demo/Demo.js";
 import { INIT_STYLE } from "./config/config.js";
 
 Core.getInstance().init(INIT_STYLE.bootstrap);
 
-client(new NewsElementCreator(), {
-    title: 'Title of news',
-    image: '../images/news.jpg',
-    text: 'Text of news',
-    link: '#'
-});
+let demo = new Demo();
 
-client(new NewsElementCreator(), {
-    title: 'Test title of news',
-    image: '../images/news.jpg',
-    text: 'Test text of news',
-    link: '#test'
-});
-
-client(new UserElementCreator(), {
-    firstName : 'Ivan',
-    lastName : 'Ivanenko',
-    avatar : '../images/avatar.jpg'
-});
-
-client(new FormElementCreator(), {});
-
-function client(creator, params) {
-    creator.createAndAppendTo(Core.getInstance().getMain(), params);
-}
+demo.demoBuilder();
+demo.demoFactoryMethod();
+demo.demoPrototype();
+demo.demoBridge();
