6f3de0868e73bf74950ec626f0db83cf5d93f76d
Oleksandr Tsvik <oleksandr.zwick@gmail.com>
2022-06-06 00:45:09 +0300

    builder

---
+++ scripts/library/Builder/Modal.js
@@ -0,0 +1,44 @@
+import { DOM } from "../DOM.js";
+
+export class Modal {
+    id;
+
+    wrapper;
+    modalDialog;
+    modalContent;
+    button;
+
+    constructor(id) {
+        this.id = id;
+
+        this.modalContent = DOM.tag({
+            name: 'div',
+            class: 'modal-content'
+        });
+
+        this.modalDialog = DOM.tag({
+            name: 'div',
+            class: 'modal-dialog',
+            content: this.modalContent
+        });
+
+        this.wrapper = DOM.tag({
+            name: 'div',
+            class: 'modal',
+            attributes: {
+                id: id,
+                tabindex: -1,
+                'aria-hidden': true
+            },
+            content: this.modalDialog
+        });
+    }
+
+    getModal() {
+        return this.wrapper;
+    }
+
+    getButton() {
+        return this.button;
+    }
+}
---
+++ scripts/library/Builder/ModalBuilder.js
@@ -0,0 +1,100 @@
+import { Modal } from "./Modal.js";
+import { DOM } from "../DOM.js";
+
+export class ModalBuilder {
+    modal;
+
+    constructor(id) {
+        this.modal = new Modal(id);
+    }
+
+    addHeader(content) {
+        if (typeof content === 'string') {
+            content = [
+                DOM.tag({
+                    name: 'h5',
+                    class: 'modal-title',
+                    content: content
+                }),
+                DOM.tag({
+                    name: 'button',
+                    class: 'btn-close',
+                    attributes: {
+                        'data-bs-dismiss': 'modal',
+                        'aria-label': 'Close'
+                    }
+                })
+            ];
+        }
+
+        this.modal.header = DOM.tag({
+            name: 'div',
+            class: 'modal-header',
+            content: content
+        });
+
+        this.modal.modalContent.append(this.modal.header);
+        return this;
+    }
+
+    addBody(content) {
+        if (typeof content === 'string') {
+            content = DOM.tag({
+                name: 'p',
+                content: content
+            });
+        }
+
+        this.modal.body = DOM.tag({
+            name: 'div',
+            class: 'modal-body',
+            content: content
+        });
+
+        this.modal.modalContent.append(this.modal.body);
+        return this;
+    }
+
+    addFooter(content) {
+        if (typeof content === 'string') {
+            content = DOM.tag({
+                name: 'button',
+                class: 'btn btn-secondary',
+                attributes: {
+                    'data-bs-dismiss': 'modal'
+                },
+                content: content
+            });
+        }
+
+        this.modal.footer = DOM.tag({
+            name: 'div',
+            class: 'modal-footer',
+            content: content
+        });
+
+        this.modal.modalContent.append(this.modal.footer);
+        return this;
+    }
+
+    setButton(content) {
+        if (typeof content === 'string') {
+            content = DOM.tag({
+                name: 'button',
+                class: 'btn btn-primary',
+                content: content
+            });
+        }
+
+        this.modal.button = content;
+
+        this.modal.button.setAttribute('data-bs-toggle', 'modal');
+        this.modal.button.setAttribute('data-bs-target', `#${this.modal.id}`);
+
+        return this;
+    }
+
+    build() {
+        return this.modal;
+    }
+}
